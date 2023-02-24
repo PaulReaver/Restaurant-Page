b8c82bbec35f218ee683153561ebeb85477e890b
Paul Vallas <vallasff@hotmail.com>
2023-02-24 15:06:43 +0200

    Create and append header

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 import '../css/reset.css';
 import '../css/style.css';
+
+const content = document.querySelector('#content');
+
+const header = document.createElement('header');
+content.appendChild(header);
+
+const restaurant = document.createElement('h1');
+restaurant.textContent = 'Restaurant';
+header.appendChild(restaurant);
