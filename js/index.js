137f5fc59ac04e85faba1e915e597acc4742ed13
Paul Vallas <vallasff@hotmail.com>
2023-02-24 21:56:34 +0200

    Append navbar items

--- js/index.js
+++ js/index.js
@@ -7,5 +7,33 @@ const header = document.createElement('header');
 body.prepend(header);
 
 const restaurant = document.createElement('h1');
-restaurant.textContent = 'Restaurant';
-header.appendChild(restaurant);
+restaurant.append('Restaurant');
+
+const nav = document.createElement('nav');
+header.append(restaurant, nav);
+
+const navlist = document.createElement('ul');
+navlist.classList.add('nav-bar');
+nav.append(navlist);
+
+const li1 = document.createElement('li');
+const li2 = document.createElement('li');
+const li3 = document.createElement('li');
+
+navlist.append(li1, li2, li3);
+
+const tab1 = document.createElement('a');
+tab1.append('Home');
+li1.append(tab1);
+
+const tab2 = document.createElement('a');
+tab2.append('Menu');
+li2.append(tab2);
+
+const tab3 = document.createElement('a');
+tab3.append('Contact');
+li3.append(tab3);
+
+tab1.href = '#';
+tab2.href = '#';
+tab3.href = '#';
