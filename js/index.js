7b22913c848cef3af00868843cc308807593eb2e
Paul Vallas <vallasff@hotmail.com>
2023-02-24 15:57:22 +0200

    Create content div and append

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 import '../css/reset.css';
 import '../css/style.css';
 
-const content = document.querySelector('#content');
+const body = document.querySelector('body');
 
 const header = document.createElement('header');
-content.appendChild(header);
+body.appendChild(header);
+
+const content = document.createElement('div');
+content.setAttribute('id', 'content');
+body.appendChild(content);
 
 const restaurant = document.createElement('h1');
 restaurant.textContent = 'Restaurant';
