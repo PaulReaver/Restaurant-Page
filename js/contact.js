571923864539b10578a0f7be388138e3d1bc87d6
Paul Vallas <vallasff@hotmail.com>
2023-02-26 15:13:06 +0200

    Replace children on select

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,7 @@
+// Get body and content div and create main element
+const content = document.querySelector('#content');
+
 export default function populateContact() {
     document.body.style.backgroundImage = "url('../images/food-contact.webp')";
+    content.replaceChildren();
 }
--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,7 @@
+// Get body and content div and create main element
+const content = document.querySelector('#content');
+
 export default function populateMenu() {
     document.body.style.backgroundImage = "url('../images/food-menu.webp')";
+    content.replaceChildren();
 }
