9dd7d81ca82ff4963360990487eace44dec83017
Paul Vallas <vallasff@hotmail.com>
2023-02-26 21:05:41 +0200

    Fixed a comment

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
-// Get body and content div and create main element
+// Get content div and create main element
 const content = document.querySelector('#content');
 
 export default function populateContact() {
