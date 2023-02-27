2d8534ba505e729626eab3ee13d4a8ac66a19393
Paul Vallas <vallasff@hotmail.com>
2023-02-27 21:32:24 +0200

    Created all days and hours

--- js/contact.js
+++ js/contact.js
@@ -4,24 +4,52 @@ const mainContact = document.createElement('main');
 mainContact.classList.add('main-contact');
 
 const openingHoursContainer = document.createElement('section');
-openingHoursContainer.classList.add('opening-hours');
+openingHoursContainer.classList.add('opening-hours-container');
 
+// Create Opening hours title
 const openingHoursTitle = document.createElement('h2');
 openingHoursTitle.classList.add('opening-hours-title');
 openingHoursTitle.append('Opening Hours');
 
+// Create Monday - Thursday title
 const mondayToThursday = document.createElement('h3');
 mondayToThursday.classList.add('day');
 mondayToThursday.append('Monday - Thursday');
 
+// Create Monday - Thursday hours
 const mondayToThursdayHours = document.createElement('p');
 mondayToThursdayHours.classList.add('hours');
 mondayToThursdayHours.append('9am - 6pm');
 
+// Create Friday - Saturday title
+const fridayToSaturday = document.createElement('h3');
+fridayToSaturday.classList.add('day');
+fridayToSaturday.append('Friday - Saturday');
+
+// Create Friday - Saturday hours
+const fridayToSaturdayHours = document.createElement('p');
+fridayToSaturdayHours.classList.add('hours');
+fridayToSaturdayHours.append('9am - 11pm');
+
+// Create Sunday title
+const sunday = document.createElement('h3');
+sunday.classList.add('day');
+sunday.append('Sunday');
+
+// Create Sunday hours
+const sundayHours = document.createElement('p');
+sundayHours.classList.add('hours');
+sundayHours.append('Closed');
+
+// Append all days and hours to container
 openingHoursContainer.append(
     openingHoursTitle,
     mondayToThursday,
-    mondayToThursdayHours
+    mondayToThursdayHours,
+    fridayToSaturday,
+    fridayToSaturdayHours,
+    sunday,
+    sundayHours
 );
 
 // Populate page
