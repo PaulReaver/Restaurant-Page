528f90b8070c648dba645a7a34c663b1786b4037
Paul Vallas <vallasff@hotmail.com>
2023-03-10 13:38:41 +0200

    test

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    build: {
-        base: '/docs/',
-        outDir: 'docs',
-    },
-});
