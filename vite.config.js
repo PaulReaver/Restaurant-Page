b377d2b4a0cbaf9bd49a86e3c8e808e762b765e4
Paul Vallas <vallasff@hotmail.com>
2023-03-10 13:29:58 +0200

    test

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        base: '/restaurant-page/',
+        outDir: 'docs',
+    },
+});
